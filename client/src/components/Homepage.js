82a0f619d5cf76484d34ccf2be88db8f67bd5058
Nicolas Ampuero <78217308+nampuero@users.noreply.github.com>
2021-11-21 09:50:42 -0500

    Fix banner text

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -42,14 +42,13 @@ const Homepage = () => {
     <div>
       {!currentUser.isAuthenticated && (
         <div>
-          <Header text={'Welcome to MAP NYC'} />
+          <Header text={'Welcome to M.A.P. NYC'} />
           <Typography variant="h5">
             A catalogue of urban farms and gardens in New York City
           </Typography>
           <Typography paragraph>
-            Are you running a farm/garden in the city?&nbsp;
-            <Link to="/signup">Sign up</Link>
-            &nbsp;to show the world what you grow!
+            Are you running a farm/garden in the city?{' '}
+            <Link to="/signup">Sign up</Link> to show the world what you grow!
           </Typography>
         </div>
       )}
